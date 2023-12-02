2609b305885634eb006e0b5364894545d273d796
Santiago Encina <santiago.encina@ing.asutral.edu.ar>
2023-12-02 13:56:48 -0300

    image url

--- scripts/server.py
+++ scripts/server.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, UnidentifiedImageError
 import io
 import face_recognition
 import uuid
@@ -8,11 +8,14 @@ from pinecone_client import upsert, query
 import tempfile
 import os
 
-
 TRESHOLD = 0.95
 
 app = Flask(__name__)
 CORS(app, support_credentials=True, resources={r"/*": {"origins": "*"}})
+
+ANNOTATED_IMAGES_DIR = 'annotated_images'
+if not os.path.exists(ANNOTATED_IMAGES_DIR):
+    os.makedirs(ANNOTATED_IMAGES_DIR)
 
 @app.route('/health')
 def home():
@@ -20,14 +23,12 @@ def home():
 
 @app.route('/upload/<name>', methods=['POST'])
 def upload_image(name):
-    image_data = request.get_data() # Get the image data
-    image = Image.open(io.BytesIO(image_data)) # Open the image
+    image_data = request.get_data()  
+    image = Image.open(io.BytesIO(image_data))  
 
-    # Save the image to a temporary file
     temp = tempfile.NamedTemporaryFile(delete=False)
     image.save(temp, format='JPEG')
 
-    # Load the image file from the temporary file
     loaded_image = face_recognition.load_image_file(temp.name)
     encoding = face_recognition.face_encodings(loaded_image)[0].tolist()
     name_content = name.split('-')
@@ -40,9 +41,9 @@ def upload_image(name):
         "metadata": {
             "id": id,
             "name": name,
-            }
-        })
-    
+        }
+    })
+
     upsert(vectors)
     temp.close()
     os.unlink(temp.name)
@@ -50,10 +51,15 @@ def upload_image(name):
 
 @app.route('/attendance', methods=['POST'])
 def attendance():
-    image_data = request.get_data()
-    image = Image.open(io.BytesIO(image_data))
+    try:
+        image_data = request.get_data()
+        image = Image.open(io.BytesIO(image_data))
+    except UnidentifiedImageError as e:
+        return jsonify({'error': 'Invalid image format'}), 400
+
     temp = tempfile.NamedTemporaryFile(delete=False)
     image.save(temp, format='JPEG')
+
     loaded_image = face_recognition.load_image_file(temp.name)
     locations = face_recognition.face_locations(loaded_image)
     encodings = face_recognition.face_encodings(loaded_image, locations)
@@ -76,14 +82,19 @@ def attendance():
             draw.text((left, top - 10), name, fill="red")
 
     temp.close()
-    os.unlink(temp.name)
 
-    # Save the annotated image to a bytes buffer
-    annotated_image_buffer = io.BytesIO()
-    image.save(annotated_image_buffer, format='JPEG')
+    # Save the annotated image to a file
+    annotated_image_filename = f"{uuid.uuid4()}_annotated.jpg"
+    annotated_image_path = os.path.join(ANNOTATED_IMAGES_DIR, annotated_image_filename)
+    image.save(annotated_image_path, format='JPEG')
 
-    # Return the annotated image and the names of attendees
-    return jsonify({'attendees': attndees, 'annotated_image': annotated_image_buffer.getvalue().hex()}), 200
+    # Return the URL of the annotated image
+    annotated_image_url = f"/get_annotated_image/{annotated_image_filename}"
+    return jsonify({'attendees': attndees, 'annotated_image_url': annotated_image_url}), 200
+
+@app.route('/get_annotated_image/<filename>')
+def get_annotated_image(filename):
+    return send_from_directory(ANNOTATED_IMAGES_DIR, filename)
 
 if __name__ == '__main__':
-    app.run(port=8000,debug=True)
+    app.run(port=8000, debug=True)
